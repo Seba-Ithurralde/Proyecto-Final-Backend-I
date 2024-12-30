9175ed5644a8ac234bcbc8f9005778819843a4cd
Sebastian Ithurralde <ithurraldesebastian@gmail.com>
2024-12-29 22:47:17 -0300

    Proyecto Final Backend I

--- proyecto-final/aggregation/product.model.js
+++ proyecto-final/aggregation/product.model.js
@@ -4,6 +4,9 @@ const productCollection = "products";
 
 const productSchema = new mongoose.Schema({
   name: String,
+  description: String,
+  thumbnail: String,
+  code: String,
   title: String,
   price: Number,
   stock: Number,
