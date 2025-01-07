b399fde708288519abc4062b8aa13060f9e3bcb0
Sebastian Ithurralde <ithurraldesebastian@gmail.com>
2025-01-07 20:42:11 -0300

    Proyecto Final Backend I

--- proyecto-final/src/config/mongoDb.config.js
+++ proyecto-final/src/config/mongoDb.config.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectMongoDB = async () => {
   try {
-    mongoose.connect("mongodb+srv://admin:123@cluster70395.glb9w.mongodb.net/proyectofinal");
+    mongoose.connect("mongodb+srv://admin:123@cluster70395.hei9p.mongodb.net/?retryWrites=true&w=majority&appName=Cluster70395");
     console.log("Mongo DB Connected");
   } catch (error) {
     console.log(error);
